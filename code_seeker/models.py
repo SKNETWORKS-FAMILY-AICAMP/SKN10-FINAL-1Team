af2dccc5f92d9ef599b3846b65135ddcc0ab9ed4
kqe123 <tnstn1155@naver.com>
2025-05-22 11:30:16 +0900

    Django로 RAG 챗봇 구현

--- code_seeker/models.py
+++ code_seeker/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
-# Create your models here.
+import uuid
+class ChatMessage(models.Model):
+    chat_id = models.UUIDField(default=uuid.uuid4, editable=False)  # 세션 구분용 고유 ID
+    is_human = models.BooleanField()
+    message = models.TextField() # 메시지 내용
+    timestamp = models.DateTimeField(auto_now_add=True)  # 대화 시간
