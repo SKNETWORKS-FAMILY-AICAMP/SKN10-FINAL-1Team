898c5316f5ae1d1e129f7e955f3e705bc34f4644
kqe123 <tnstn1155@naver.com>
2025-05-29 12:43:17 +0900

    챗봇 세션 추가2

--- code_seeker/views.py
+++ code_seeker/views.py
@@ -8,8 +8,27 @@ from langchain.vectorstores import Chroma
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import RetrievalQA
 
+"""
+1. get 방식으로 파라미터 입력받음
+2. 파라미터가 있다면 해당 세션 선택, 없다면 세션들중에 가장 최근의 세션 선택
+3. 세션 자체가 아무것도 없다면 새 세션을 생성 (세션 title은 "새 세션")
+4. 해당 세션으로 사용자 입력을 메시지로 넣고, 챗봇 응답도 메세지로 넣음.
+
+"""
+
+
+
 # Create your views here.
 def index(request) :
+    session_id = request.GET.get('session_id') # GET 파라미터가 없다면, request.GET.get()은 기본적으로 None을 반환
+    if session_id is None : 
+        if ChatSession.objects.exists() : # 남은 세션이 1개라도 있다면 가장 최근의 세션을 선택
+            session_id = ChatSession.objects.order_by('-started_at').first().id
+        else : # 남은 세션이 없을때, 새 세션을 생성하여 선택
+            new_session = ChatSession.objects.create()
+            session_id = new_session.id
+    
+
     if request.method == 'POST' :
         # 유저 메시지 저장
         
@@ -44,7 +63,7 @@ def index(request) :
         chatbot_chat = ChatMessage(is_human=False, message=result['result'])
         chatbot_chat.save()
 
-    content = {"messages" : ChatMessage.objects.all().order_by('timestamp')}
+    content = {"messages" : ChatMessage.objects.all().order_by('created_at')}
     return render(request, "code_seeker/index.html", content)
 
 def delete_message(request) : 
