38ad8926bf7439d4d1c49dbc7224baf1e95b4b02
HIHO999 <in9045@naver.com>
2025-07-01 20:25:40 +0900

    포트 수정

--- fastapi_server/main.py
+++ fastapi_server/main.py
@@ -175,6 +175,6 @@ def read_root():
 # --- Uvicorn Runner ---
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8001, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
 
 
